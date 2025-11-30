ca6395f33742728930fae1d23a108ea061ed6693
Canaan <canaan.white5@gmail.com>
2025-11-30 14:41:48 -0800

    first commit

---
+++ Api/Models/LogItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Models;
+
+public class LogItem
+{
+    public Guid Id { get; set; }
+    public string Discrepancy { get; set; } = string.Empty;
+    public bool SignedOff { get; set; } = false;
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+    public DateTime? SignedOffOn { get; set; } = null;
+}
---
+++ Api/Services/LogItemService.cs
@@ -0,0 +1,44 @@
+﻿using Api.Models;
+namespace Api.Services;
+
+public class LogItemService
+{
+    private static List<LogItem> LogItems { get; }
+
+    static LogItemService()
+    {
+        LogItems = new List<LogItem>
+        {
+            new LogItem { Id = Guid.NewGuid(), Discrepancy = "RH Fuel Pump Inop", CreatedOn = DateTime.Now, SignedOff = false, SignedOffOn = null},
+            new LogItem { Id = Guid.NewGuid(), Discrepancy = "HF Static", CreatedOn = DateTime.Now, SignedOff = false, SignedOffOn = null}
+        };
+    }
+
+    public static List<LogItem> GetAll() => LogItems;
+
+    public static LogItem? Get(Guid id) => LogItems.FirstOrDefault(i => i.Id == id);
+
+    public static void AddLogItem(LogItem newItem)
+    {
+        newItem.Id = Guid.NewGuid();
+        LogItems.Add(newItem);
+    }
+
+    public static void Delete(Guid id)
+    {
+        var logItem = Get(id);
+        if (logItem is null)
+            return;
+        
+        LogItems.Remove(logItem);
+    }
+
+    public static void Update(LogItem logItem)
+    {
+        var index = LogItems.FindIndex(i => i.Id == logItem.Id);
+        if (index == -1)
+        {
+            LogItems[index] = logItem;
+        }
+    }
+}
